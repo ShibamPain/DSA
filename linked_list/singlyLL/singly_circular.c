4bd6638fa83c00d0bede5e6e525bb8f44662b9d3
ShibamPain <maxxpayne3006@gmail.com>
2025-08-10 12:40:44 +0530

    ouk

--- linked_list/singlyLL/singly_circular.c
+++ linked_list/singlyLL/singly_circular.c
@@ -8,12 +8,11 @@ typedef struct Node
 
 } node;
 
-node *head = NULL,*tail=NULL;
+node *head = NULL, *tail = NULL;
 void inrt_at_begin(int);
 node *makenode(int);
 void display();
 void delete(node *);
-
 
 int main()
 {
@@ -30,19 +29,18 @@ int main()
 void inrt_at_begin(int data)
 {
     node *newnode = makenode(data);
-    
 
-    // NOW I HAVE TO INSERT THIS NODE AT THE BEGGINING BUT THIS IS CIRCULAR 
-    if(!head)
+    // NOW I HAVE TO INSERT THIS NODE AT THE BEGGINING BUT THIS IS CIRCULAR
+    if (!head)
     {
-        head=tail = newnode;
+        head = tail = newnode;
     }
     else
     {
-        newnode->nxt=head;
-        head=newnode;
+        newnode->nxt = head;
+        head = newnode;
     }
-        tail->nxt=newnode;
+    tail->nxt = newnode;
 }
 
 void display()
@@ -57,34 +55,41 @@ void display()
             printf("%d ", (*temp).data);
             temp = temp->nxt;
         }
-        printf("%d",temp->data);
+        printf("%d", temp->data);
     }
 }
 void delete(node *curr) // delete(head)
 {
-    if (curr == NULL)// BASE CONDITION..:)
+    if (curr == NULL) // BASE CONDITION..:)
         return;
+    if (head == tail)
+    {
+        free(head);
+        head = tail = NULL;
+    }
+    while (head != tail)
+    {
+        node *current = curr; // current = head = address od 1st node
 
-    node *current = curr; // current = head = address od 1st node
+        node *address_of_next_node = (*current).nxt; // address_of_next_node = address of 2nd node
 
-    node *address_of_next_node = (*current).nxt; // address_of_next_node = address of 2nd node
-
-    free(current); // free the 1st node
-
-    delete(address_of_next_node);
-    head = NULL;
+        free(current); // free the 1st node
+        head = address_of_next_node;
+        tail->nxt = address_of_next_node;
+        delete(address_of_next_node);
+    }
 }
-//MAKING NEW NODE 
+// MAKING NEW NODE
 
 node *makenode(int data)
 {
-    node *newnode = malloc(sizeof (node));
-    if(newnode == NULL)
+    node *newnode = malloc(sizeof(node));
+    if (newnode == NULL)
     {
         printf("memory allocation failed");
         exit(1);
     }
-        newnode->data = data;
-        newnode->nxt = NULL;
-        return newnode ; 
+    newnode->data = data;
+    newnode->nxt = NULL;
+    return newnode;
 }
